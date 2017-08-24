d5c33ae68f83d51740274410bdf2f140a86f2c98
Chris Grocott <chris.grocott@gmail.com>
2017-08-24 23:51:00 +0100

    Began implementation for getting /users/{id} resource

--- src/main/java/uk/co/grokemon/flipside/web/UserController.java
+++ src/main/java/uk/co/grokemon/flipside/web/UserController.java
@@ -2,6 +2,7 @@ package uk.co.grokemon.flipside.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import uk.co.grokemon.flipside.domain.User;
 import uk.co.grokemon.flipside.service.UserService;
@@ -17,5 +18,11 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers() {
         return userService.getUserList();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable("id") int id) {
+        //TODO this isn't quite what I want, how to return a 404 if no user is found?
+        return userService.getUserList().stream().filter(user -> user.getId() == id).findFirst().orElse(new User());
     }
 }
